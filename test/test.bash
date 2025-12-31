4eb53711aa4fba9267ca185a4542860a08cf5b86
Ryoichi Sakamaki <gerggms14@gmail.com>
2025-12-31 18:07:18 +0900

    e

--- test/test.bash
+++ test/test.bash
@@ -4,14 +4,16 @@ dir=~
 [ "$1" != "" ] && dir="$1"
 
 
+# ROS2環境設定
+source /opt/ros/humble/setup.bash
+
 cd $dir/ros2_ws
 colcon build
 
-# ROS2環境設定
-source /opt/ros/humble/setup.bash
 source $dir/ros2_ws/install/setup.bash
 
 timeout 10 ros2 launch mypkg mic_to_piano.launch.py > /tmp/mypkg.log
 
-cat /tmp/mypkg.log | grep "from mic_freq_pub to tuner_node"
-cat /tmp/mypkg.log | grep "from tuner_node to draw_piano"
+cat /tmp/mypkg.log | grep "audio stream started"
+#cat /tmp/mypkg.log | grep "from mic_freq_pub to tuner_node"
+#cat /tmp/mypkg.log | grep "from tuner_node to draw_piano"
