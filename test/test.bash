7d6e7f52315ef540b0b9db609e78421bac31b098
Ryoichi Sakamaki <gerggms14@gmail.com>
2025-12-31 18:24:09 +0900

    r

--- test/test.bash
+++ test/test.bash
@@ -1,4 +1,6 @@
 #!/bin/bash
+# SPDX-FileCopyrightText: 2025 Ryoichi Sakamaki
+# SPDX-License-Identifier: GPL-3.0-only
 
 dir=~
 [ "$1" != "" ] && dir="$1"
@@ -14,6 +16,6 @@ source $dir/ros2_ws/install/setup.bash
 
 timeout 10 ros2 launch mypkg mic_to_piano.launch.py > /tmp/mypkg.log
 
-cat /tmp/mypkg.log | grep "audio stream started"
+#cat /tmp/mypkg.log | grep "audio stream started"
 #cat /tmp/mypkg.log | grep "from mic_freq_pub to tuner_node"
 #cat /tmp/mypkg.log | grep "from tuner_node to draw_piano"
