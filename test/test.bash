b941d7e41d19019d04cd34eabea7561c380f4aae
Ryoichi Sakamaki <gerggms14@gmail.com>
2026-01-01 20:56:02 +0900

    私はgithubactionsを許さぬ

--- test/test.bash
+++ test/test.bash
@@ -11,6 +11,10 @@ source /opt/ros/humble/setup.bash
 source $dir/ros2_ws/install/setup.bash
 
 timeout 10 ros2 launch mypkg mic_to_piano.launch.py > /tmp/mypkg.log
+cat /tmp/mypkg.log
+ng () {
+	res=1
+}
 res=0
 cat /tmp/mypkg.log | grep -q 'audio stream started'
 [ "$?" = 0 ] || [ res=1 ]
