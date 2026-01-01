82e7aa7db124eeb4bf602b2a03aa11b4bccc5465
Ryoichi Sakamaki <gerggms14@gmail.com>
2026-01-01 12:57:33 +0900

    s

--- test/test.bash
+++ test/test.bash
@@ -1,6 +1,22 @@
 #!/bin/bash
 # SPDX-FileCopyrightText: 2025 Ryoichi Sakamaki
 # SPDX-License-Identifier: GPL-3.0-only
-set -e
-python3 -m compileall .
-colcon build --packages-select mypkg
+
+dir=~
+[ "$1" != "" ] && dir="$1"
+
+cd $dir/ros2_ws
+colcon build
+# ROS2環境設定
+source /opt/ros/humble/setup.bash
+source $dir/ros2_ws/install/setup.bash
+
+timeout 10 ros2 launch mypkg mic_to_piano.launch.py > /tmp/mypkg.log
+#timeout 10 ros2 run mypkg mic_freq_pub
+#timeout 10 ros2 launch mypkg tuner_node > /tmp/mypkg.log
+#timeout 10 ros2 launch mypkg draw_piano > /tmp/mypkg.log
+
+cat /tmp/mypkg.log
+# | grep "audio stream started"
+#cat /tmp/mypkg.log | grep "from mic_freq_pub to tuner_node"
+#cat /tmp/mypkg.log | grep "from tuner_node to draw_piano"
