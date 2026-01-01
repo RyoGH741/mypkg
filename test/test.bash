a6088ab684f12fd3911d18726584b7300fc7b205
Ryoichi Sakamaki <gerggms14@gmail.com>
2026-01-01 21:22:30 +0900

    我githubactionsを許さぬ

--- test/test.bash
+++ test/test.bash
@@ -11,18 +11,17 @@ source /opt/ros/humble/setup.bash
 source $dir/ros2_ws/install/setup.bash
 
 timeout 10 ros2 launch mypkg mic_to_piano.launch.py > /tmp/mypkg.log
-python3 -m sounddevice
-cat /tmp/mypkg.log
+
 ng () {
 	res=1
 }
 res=0
 cat /tmp/mypkg.log | grep -q 'audio stream started'
-[ "$?" = 0 ] || [ res=1 ]
+[ "$?" = 0 ] || ng
 cat /tmp/mypkg.log | grep -q 'from mic_freq_pub to tuner_node'
-[ "$?" = 0 ] || [ res=1 ]
+[ "$?" = 0 ] || ng
 cat /tmp/mypkg.log | grep -q 'from tuner_node to draw_piano'
-[ "$?" = 0 ] || [ res=1 ]
+[ "$?" = 0 ] || ng
 
 [ "${res}" = 0 ] && echo OK
 
