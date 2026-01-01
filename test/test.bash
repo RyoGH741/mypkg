f699fec0669e60444e56b22a815a1f94ebed2ee3
Ryoichi Sakamaki <gerggms14@gmail.com>
2026-01-01 18:14:21 +0900

    githubactionに音声デバイスないよ

--- test/test.bash
+++ test/test.bash
@@ -1,8 +1,6 @@
 #!/bin/bash
 # SPDX-FileCopyrightText: 2025 Ryoichi Sakamaki
 # SPDX-License-Identifier: GPL-3.0-only
-export PULSE_SERVER=unix:/run/user/1001/pulse/native
-ls -l /run/user/*/pulse/native
 
 dir=~
 [ "$1" != "" ] && dir="$1"
@@ -10,14 +8,10 @@ python3 -m sounddevice
 
 cd $dir/ros2_ws
 colcon build
-# ROS2環境設定
 source /opt/ros/humble/setup.bash
 source $dir/ros2_ws/install/setup.bash
 
 timeout 10 ros2 launch mypkg mic_to_piano.launch.py > /tmp/mypkg.log
-#timeout 10 ros2 run mypkg mic_freq_pub
-#timeout 10 ros2 launch mypkg tuner_node > /tmp/mypkg.log
-#timeout 10 ros2 launch mypkg draw_piano > /tmp/mypkg.log
 
 cat /tmp/mypkg.log
 # | grep "audio stream started"
